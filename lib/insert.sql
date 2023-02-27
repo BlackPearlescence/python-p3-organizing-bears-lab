6edaeb7578804f88fe84b54d0c41eab760a3ce4f
Sani Rogers <sanirogers138@gmail.com>
2023-02-27 09:42:00 -0500

    do bears

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (0,"Mr.Chocolate",220,"Male","Black","Loud",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (1,"Rowdy",300,"Female","White","Violent",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (2,"Tabitha",223,"Female","Brown","Bloodthirsty",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (3,"Sergeant Brown",50,"Male","Blue","Cutthroat",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (4,"Melissa",30,"Female","Purple","Malicious",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (5,"Grinch",1,"Male","Green","Vile",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (6,"Wendy",1,"Female","Ochre","Evil",1);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES (7,NULL,0,"Male","Colorless","Serene",0)
